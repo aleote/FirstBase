e352a60c7d4f1e156904d0fe7ad090d909e5d2d2
ctabones <ctabones@gmail.com>
2017-10-13 14:53:38 -0400

    added route

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,9 +1,22 @@
+// var db = require("../models/user.js");
 var db = require("../models");
 
 module.exports = function(app) {
-	app.get("/", function(req, res) {
+	app.get("/", function(res) {
 		db.User.findAll({}).then(function(result){
-			console.log(result)
+		console.log(result[0].username)
 		});
+		//res.send
 	});
 };
+
+
+
+// 	 {
+// 		db.User.findAll({}).then(function(result){
+// 			var something = res.json(result)
+
+// 			console.log(something);
+// 		});
+// 	});
+// };
